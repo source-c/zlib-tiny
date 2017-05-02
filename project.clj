b40573a885331d09221a6729c4a7a36624eaacd3
MelKori <mieldo@gmail.com>
2017-05-02 20:16:56 +0300

    initial commit -- empty repo

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject net.tbt-post/zlib-tiny "0.1.0-SNAPSHOT"
+  :description "Tiny Clojure ZLib helper"
+  :url "https://github.com/source-c/zlib-tiny"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
