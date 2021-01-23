1aedd5b0a38a5a6776b800f8eea423b17de8482d
MelKori <mieldo@gmail.com>
2021-01-23 21:18:46 +0200

    update project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject net.tbt-post/zlib-tiny "0.2.3"
+(defproject net.tbt-post/zlib-tiny "0.2.4"
   :description "Tiny Clojure ZLib helper"
   :url "https://github.com/source-c/zlib-tiny"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[commons-io "2.6"]])
+  :dependencies [[commons-io "2.8.0"]])
