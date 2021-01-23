2f64057759816fbc5cf10dca90c6c126a3f6cefd
MelKori <mieldo@gmail.com>
2021-01-23 21:18:38 +0200

    refactor profiles

--- profiles.clj
+++ profiles.clj
@@ -1,22 +1,14 @@
-{:dev     {:global-vars         {*warn-on-reflection* true}
+{:dev     {:global-vars    {*warn-on-reflection* true}
 
-           :source-paths        #{"src"}
-           :resource-paths      ["resources"]
+           :source-paths   #{"src"}
+           :resource-paths ["resources"]
 
-           :target-path         "target/%s"
-           :clean-targets       ^{:protect false} [:target-path]
+           :target-path    "target/%s"
+           :clean-targets  ^{:protect false} [:target-path]
 
-           :plugins             [[lein-ancient "0.6.15"]
-                                 [org.apache.maven.wagon/wagon-ssh-external "3.0.0"]
-                                 [org.apache.maven.wagon/wagon-http-lightweight "3.0.0"]]
+           :plugins        [[org.apache.maven.wagon/wagon-ssh-external "3.4.2"]
+                            [org.apache.maven.wagon/wagon-http-lightweight "3.4.2"]]}
 
-           :dependencies        [[org.clojure/clojure "1.10.0"]]}
+ :provided {:dependencies [[org.clojure/clojure "1.10.1"]]}
 
- :uberjar {:aot      :all
-           :jvm-opts #=(eval
-                         (concat ["-Xmx1G"]
-                           (let [version (System/getProperty "java.version")
-                                 [major _ _] (clojure.string/split version #"\.")]
-                             (if (>= (Integer. major) 9)
-                               ["--add-modules" "java.xml.bind"]
-                               []))))}}
+ :jar     {:aot :all}}
