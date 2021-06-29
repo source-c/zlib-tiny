2958d0087610e2d6d6ecbba0b103694d70919dbd
MelKori <mieldo@gmail.com>
2021-06-29 12:12:12 +0300

    update dev deps

--- profiles.clj
+++ profiles.clj
@@ -6,9 +6,9 @@
            :target-path    "target/%s"
            :clean-targets  ^{:protect false} [:target-path]
 
-           :plugins        [[org.apache.maven.wagon/wagon-ssh-external "3.4.2"]
-                            [org.apache.maven.wagon/wagon-http-lightweight "3.4.2"]]}
+           :plugins        [[org.apache.maven.wagon/wagon-ssh-external "3.4.3"]
+                            [org.apache.maven.wagon/wagon-http-lightweight "3.4.3"]]}
 
- :provided {:dependencies [[org.clojure/clojure "1.10.1"]]}
+ :provided {:dependencies [[org.clojure/clojure "1.10.3"]]}
 
  :jar     {:aot :all}}
