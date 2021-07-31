375d8b482fbc818e799da4cfd654b4b8c8901f6d
MelKori <mieldo@gmail.com>
2021-08-01 01:00:17 +0300

    added crc64, md5, sha1, sha256

--- profiles.clj
+++ profiles.clj
@@ -1,14 +1,19 @@
-{:dev     {:global-vars    {*warn-on-reflection* true}
+{:dev      {:global-vars       {*warn-on-reflection* true}
 
-           :source-paths   #{"src"}
-           :resource-paths ["resources"]
+            :source-paths      #{"src-clj"}
+            :java-source-paths #{"src-java"}
+            :resource-paths    ["resources"]
 
-           :target-path    "target/%s"
-           :clean-targets  ^{:protect false} [:target-path]
+            :jar-exclusions    [#"\.java"]
 
-           :plugins        [[org.apache.maven.wagon/wagon-ssh-external "3.4.3"]
-                            [org.apache.maven.wagon/wagon-http-lightweight "3.4.3"]]}
+            :target-path       "target/%s"
+            :clean-targets     ^{:protect false} [:target-path]
+
+            :dependencies      [[commons-codec "1.15"]]
+
+            :plugins           [[org.apache.maven.wagon/wagon-ssh-external "3.4.3"]
+                                [org.apache.maven.wagon/wagon-http-lightweight "3.4.3"]]}
 
  :provided {:dependencies [[org.clojure/clojure "1.10.3"]]}
 
- :jar     {:aot :all}}
+ :jar      {:aot :all}}
