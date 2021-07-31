a09e5d29fd7dfa1f9b39ee5f3fa7190640edb239
MelKori <mieldo@gmail.com>
2021-08-01 01:25:26 +0300

    fix build

--- profiles.clj
+++ profiles.clj
@@ -1,11 +1,5 @@
 {:dev      {:global-vars       {*warn-on-reflection* true}
 
-            :source-paths      #{"src-clj"}
-            :java-source-paths #{"src-java"}
-            :resource-paths    ["resources"]
-
-            :jar-exclusions    [#"\.java"]
-
             :target-path       "target/%s"
             :clean-targets     ^{:protect false} [:target-path]
 
@@ -14,6 +8,11 @@
             :plugins           [[org.apache.maven.wagon/wagon-ssh-external "3.4.3"]
                                 [org.apache.maven.wagon/wagon-http-lightweight "3.4.3"]]}
 
- :provided {:dependencies [[org.clojure/clojure "1.10.3"]]}
+ :provided {:dependencies      [[org.clojure/clojure "1.10.3"]]
+            :source-paths      #{"src-clj"}
+            :java-source-paths #{"src-java"}
+            :resource-paths    ["resources"]
+
+            :jar-exclusions    [#"\.java"]}
 
  :jar      {:aot :all}}
