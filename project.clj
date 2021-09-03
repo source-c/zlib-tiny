a8d6f4850fd560138a5f07576849f3c4394fb277
source-c <source-c@users.noreply.github.com>
2021-09-04 00:02:59 +0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject net.tbt-post/zlib-tiny "0.3.2"
   :description "Tiny Clojure ZLib helper"
   :url "https://github.com/source-c/zlib-tiny"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License v2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[commons-io "2.11.0"]])
