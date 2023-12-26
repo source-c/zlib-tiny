88a2de14479a5731a394f6975ecf534192498e85
MelKori <mieldo@gmail.com>
2023-12-26 16:04:33 +0200

    update commons-io

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject net.tbt-post/zlib-tiny "0.5.1"
+(defproject net.tbt-post/zlib-tiny "0.5.2"
   :description "Tiny Clojure ZLib helper"
   :url "https://github.com/source-c/zlib-tiny"
   :license {:name "Apache License v2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[commons-io "2.13.0"]])
+  :dependencies [[commons-io "2.15.1"]])
